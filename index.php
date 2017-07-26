4a09575c6eb1bf0f58c8092c6ab75a0e760ebcd7
Rupendra <rupu.engg@gmail.com>
2017-07-26 21:02:07 +0530

    Initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html ng-app="rup">
+<html>
+	<head>
+		<title>Move Item To List</title>
+		<meta content="text/html;charset=utf-8" http-equiv="Content-Type"/>
+		<meta content="utf-8" http-equiv="encoding"/>
+		<script type="text/javascript" src="angular.js"></script>
+		<script type="text/javascript" src="main-controller.js"></script>
+	</head>
+	<body ng-controller="mainController">
+		<h1>Move Item To List</h1>
+		<div class="move-list-item">
+		</div>
+	</body>
+</html>
---
+++ main-controller.js
@@ -0,0 +1,11 @@
+;(function(ng){
+	ng
+	.module('rup', [
+	])
+	.controller('mainController', [
+		'$scope',
+		function($scope){
+			console.log('Main Controller');
+		}
+	]);
+})(window.angular);
